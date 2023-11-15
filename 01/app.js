15dea853e0a2ae277bd39046d8b059b410a05e5b
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-15 23:50:40 +0100

    solve exercise 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+if (randomNumber >= 5) {
+    for (let i = 5; i < randomNumber; i++) {
+        console.log(i);
+    }
+
+} else {
+    console.log('wylosowana liczba jest zbyt mala, aby uzyc petli')
+}
