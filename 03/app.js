60f81cea1641e4b2239f1d39a031074326c685c4
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-16 00:32:46 +0100

    solve exercise 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+
+while (randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+    console.log("iteracja #", iteration);
+}
+console.log("sumaryczna liczba potrzebnych iteracji:", iteration);
