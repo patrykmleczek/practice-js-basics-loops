dae4cb0290174f31e5d24db1557ae4d54a6a9a8b
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-16 00:06:57 +0100

    solve exercise 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,13 @@
 const x = 5;
 let result = 0;
+
+// for (let i = result; i <= x; i++) {
+//     result += i;
+//     console.log(result)
+// }
+let i = 0
+while (i <= x) {
+    result += i;
+    i++;
+    console.log(result)
+}
